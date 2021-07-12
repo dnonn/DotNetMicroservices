6451a35abd787c91b60d9edb761b4d631c7ee70a
David Nonn <dnonn@aurasolutionsllc.com>
2021-07-12 15:29:13 -0700

    Consume checkout command. Updating docker files.

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -47,6 +47,9 @@ namespace Ordering.API
             // General configuration.
             services.AddApplicationServices();
             services.AddInfrastructureServices(Configuration);
+            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<BasketCheckoutConsumer>();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
